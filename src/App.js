52b1b7b39db07dc0d433c12400edfd1ec5bd3018
Hmoein87 <hosseinmoeini87@gmail.com>
2024-02-17 19:51:22 -0400

    rendered categories by creating category component and binded products to it

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import './App.css';
 import React, {useState, useEffect} from 'react';
 
+import Category from './Components/Category';
+import './App.css';
+
+
 function App() {
-  const [result, setResult] = useState([]);
+  const [categories, setCategories] = useState([]);
+  const [products, setProducts] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:3001/categories")
     .then(response => response.json())
     .then(data => {
       console.log(data);
-      setResult(data);
+      setCategories(data);
     })
   }, [])
+
+  const handleCategoryClick = (id) => {
+    fetch("http://localhost:3001/products?catId=" + id)
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+      setProducts(data);
+    })
+  }
+
+  const renderCategories = () => {
+    return categories.map(c => 
+      <Category key={c.id} id={c.id} title={c.title} onCategoryClick={() => handleCategoryClick(c.id)}/>
+    );
+  }
+
+  const renderProducts = () => {
+    return products.map(p =>
+      <div key={p.id}>{p.title}</div>
+    );
+  }
+
+
   return (
     <>
     <header>My Store</header>
 
     <section>
       <nav>
-        {
-          result.map(d => (
-            <div key={d.id}>{d.title}</div>
-          ))
-        }
+        { categories && renderCategories() }
       </nav>
 
       <article>
-        main area
+        <h1>Products</h1>
+        { products && renderProducts()}
       </article>
     </section>
     
