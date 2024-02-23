9bae6e43dbd7183827037ab764506803c76cbbd5
Hmoein87 <hosseinmoeini87@gmail.com>
2024-02-23 14:48:03 -0400

    styled ProductDetails Component using styled component

--- src/Components/ProductDetails.js
+++ src/Components/ProductDetails.js
@@ -1,13 +1,16 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router';
+import styled from 'styled-components';
+
 import { getProductsById } from '../fetchAPI';
+
 
 //prodcut details component
 const ProductDetails = () => {
   //product id parameter got by parameters in url
   const {productId} = useParams();
   //product propery
-  const [product, setProduct] = useState({errormessage: '', data: []})
+  const [product, setProduct] = useState({errormessage: '', data: {}})
 
   //fetch product by id from API
   useEffect(() => {
@@ -21,59 +24,134 @@ const ProductDetails = () => {
 
   return (
     <>
-        <div className='category-product-title'>
+        <ProductTitle>
             {product.data.title}
-        </div>
+        </ProductTitle>
 
-        <article>
-            <figure className='category-product-item'>
-                <div className='category-product-image-container'>
-                    <img className='product-image' src={`../assets/${product.data.image}`} alt={product.data.title}/>
-                </div>
+        <ProductItem>
+            <figure>
+                <ProductImageContainer>
+                    <ProductImageContainerImage src={`../assets/${product.data.image}`} alt={product.data.title}/>
+                </ProductImageContainer>
             </figure>
             
-            <aside className='category-product-spec category-product-item'>
-                <div className='category-product-info-dimesnions'>
+            <ProductSpecs>
+                <div>
                     <h3>Dimensions</h3>
-                    <label className='dimension'>{product.data.specs?.dimensions}</label>
+                    <ProductLabels>{product.data.specs?.dimensions}</ProductLabels>
                 </div>
 
                 {product.data.specs?.capacity &&
-                <div className='category-product-info-capacity'>
+                <div>
                     <h3>Capacity</h3>
-                    <label>{product.data.specs?.capacity}</label>
+                    <ProductLabels>{product.data.specs?.capacity}</ProductLabels>
                 </div>
                 }
 
-                <div className='category-product-info-features'>
+                <div>
                     <h3>Features</h3>
-                    <ul>
+                    <ProductFeatureUl>
                         {product.data.features?.map((f, i) => {
                             return <li key={`feature_${i}`}>{f}</li>
                         })}
-                    </ul>
+                    </ProductFeatureUl>
                 </div>
-            </aside>
+            </ProductSpecs>
 
-            <aside className='category-product-finance category-product-item'>
-                <div className='category-product-price'>
+            <ProductFinance>
+                <ProductPrice>
                 &#36;CAD {product.data.price}
-                </div>
+                </ProductPrice>
 
-                <div className='category-product-info-stock'>
+                <ProductStock>
                     <label>Stock Level: {product.data.stock}</label>
                     <label>Free Delivery</label>
-                </div>
+                </ProductStock>
 
-                <div className='category-product-action'>
+                <ProductAction>
                     <button>Add to Cart</button>
-                </div>
-            </aside>
-        </article>
+                </ProductAction>
+            </ProductFinance>
 
-        <div>{product.data?.description}</div>
+            <ProductDescription>{product.data.description}</ProductDescription>
+
+        </ProductItem>
     </>
   )
 }
 
 export default ProductDetails;
+
+//style CategoryProduct using style component
+const ProductTitle = styled.div`
+    padding-top: 20px;
+    padding-left: 20px;
+    font-size: 20px;
+    font-weight: bold;
+    color: darkgreen;
+`;
+
+const ProductItem = styled.article`
+    display: grid;
+    grid-template-columns: 0.5fr 0.75fr 0.5fr;
+    margin-bottom: 40px;
+`;
+
+const ProductImageContainer = styled.div`
+    padding-top: 10px;
+    padding-left: 20px;
+    text-align: left;
+`;
+
+const ProductImageContainerImage = styled.img`
+    width: 250px;
+    height: auto;
+`;
+
+const ProductSpecs = styled.aside`
+    padding: 5px;
+`;
+
+const ProductLabels = styled.label`
+    display: block;
+    padding: 10px;
+`;
+
+const ProductFeatureUl = styled.ul`
+    padding-left: 25px;
+    padding-top: 10px;
+`;
+
+const ProductFinance = styled.aside`
+    margin-top: 10%;
+    margin-left: 5%;
+`;
+
+const ProductPrice = styled.div`
+    font-size: 25px;
+    font-weight: bold;
+`;
+
+const ProductStock = styled.div`
+    display: grid;
+    grid-template-rows: 2;
+    grid-gap: 10px;
+    margin-top: 20px;
+    background-color: cornflowerblue;
+    width: 130px;
+`;
+
+const ProductAction = styled.div`
+    display: grid;
+    grid-template-rows: 2;
+    grid-gap: 20px;
+    margin-top: 20px;
+    width: 100px;
+`;
+
+const ProductDescription = styled.div`
+    grid-column: 1 / span 3;
+    padding: 20px;
+    padding-right: 15%;
+    text-align: justify;
+`;
